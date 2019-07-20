43d26672591189b1fc0c3311cd65e2b707f60c2b
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2019-07-20 02:19:02 -0400

    Add support for chaining

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ const {ShiftRegister} = require("rpio-shift");
 const sr = new ShiftRegister({
 	// pins: [data, clk, outputLatch]
 	pins: [11, 12, 13]
+	// How many shift registers you're chaining defaults to 1
+	registers: 2
 })
 
 // sr.write(outputPin, state)
@@ -16,9 +18,19 @@ const sr = new ShiftRegister({
 // state can be 0 or 1 (rpio.LOW or rpio.HIGH)
 sr.write(0, rpio.HIGH); // Turn on the first output pin
 sr.write(7, rpio.Low); // Turn off the last output pin
+// If you have multiple shift-registers in a chain, pin 0 is the first output pin in the last shift-register in the chain, pin 8 is the first output pin in the second-last shift-register in the chain, and so on
+
+// sr.writeState(values);
+// Sets the state of all the pins in a shift-register chain
+sr.writeState(0); // Turn off all outputs
+sr.writeState(0b11110000); // Turn off pins 0-3, turn on pins 4-7
+sr.writeState([null, 1, 0]); // Don't change the state of pin 0, turn on pin 1, turn off pin 2, don't change the state of other pins
+sr.writeState(Buffer.from(0xff, 0x0)); // Turn on all pins on the last shift-register, turn off all pins on the first shift-register
+sr.writeState(268435456n) // Turn on the 29th pin in the chain. (Using bigints)
 
 // sr.writeByte(values);
 // Takes an 8 bit uint or an array of states
+// Do not use this function if you're chaining multiple shift-registers. Use writeState instead.
 sr.writeByte(0); // Turn off all outputs
 sr.writeByte(0b11110000); // Turn off pins 0-3, turn on pins 4-7
 sr.writeByte([null, 1, 0]); // Don't change the state of pin 0, turn on pin 1, turn off pin 2, don't change the state of other pins
--- index.js
+++ index.js
@@ -11,12 +11,13 @@ class ShiftRegister {
 		this.isShiftRegister = true;
 		this.pins = [options.pins[0], options.pins[1], options.pins[2]];
 		this.io = [options.io[0] || rpio, options.io[1] || rpio, options.io[2] || rpio];
-		this.currentValue = 0b00000000;
 		this.io[0].open(this.pins[0], rpio.OUTPUT);
 		this.io[1].open(this.pins[1], rpio.OUTPUT);
 		if (this.pins[2] != null){
-			options.io[2].open(options.pins[2], rpio.OUTPUT);
+			this.io[2].open(options.pins[2], rpio.OUTPUT);
 		}
+		this.rCount = (options.registers | 0) || 1;
+		this.currentValues = Buffer.alloc(this.rCount, 0xff)
 	}
 	open() {
 
@@ -31,7 +32,8 @@ class ShiftRegister {
 		this.io[2].write(this.pins[2], rpio.HIGH);
 		this.io[2].write(this.pins[2], rpio.LOW);
 	}
-	writeByte(values){
+	writeByte(values, register, dontApply){
+		register = register | 0;
 		let pinsToKeep;
 		let newValue = 0b00000000;
 		if (Array.isArray(values)){
@@ -46,24 +48,72 @@ class ShiftRegister {
 			pinsToKeep = 0b00000000;
 			newValue = values & 0b11111111;
 		}
-		if (this.currentValue === newValue){
+		
+		if (this.currentValues[register] === newValue && this.rCount === 1){
 			return;
 		}
-		this.currentValue = (this.currentValue & pinsToKeep) | newValue;
-		this.pulse(((this.currentValue & 0b10000000) > 0) | 0);
-		this.pulse(((this.currentValue & 0b01000000) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00100000) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00010000) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00001000) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00000100) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00000010) > 0) | 0);
-		this.pulse(((this.currentValue & 0b00000001) > 0) | 0);
+		
+		const currentValue = (this.currentValues[register] & pinsToKeep) | newValue;
+		this.pulse(((currentValue & 0b10000000) > 0) | 0);
+		this.pulse(((currentValue & 0b01000000) > 0) | 0);
+		this.pulse(((currentValue & 0b00100000) > 0) | 0);
+		this.pulse(((currentValue & 0b00010000) > 0) | 0);
+		this.pulse(((currentValue & 0b00001000) > 0) | 0);
+		this.pulse(((currentValue & 0b00000100) > 0) | 0);
+		this.pulse(((currentValue & 0b00000010) > 0) | 0);
+		this.pulse(((currentValue & 0b00000001) > 0) | 0);
+		this.currentValues[register] = currentValue;
+		if (!dontApply){
+			this.apply();
+		}
+	}
+	writeState(values) {
+		if (Array.isArray(values)){
+			const bits = this.rCount * 8;
+			let register = 0;
+			for (let i = 0; i < bits; i += 8){
+				this.writeByte(values.slice(i, i + 8), register, true);
+				register += 1;
+			}
+		}else if(values instanceof Uint8Array){
+			if (values.length !== this.rCount){
+				throw new Error("Length of buffer must equal number of shift registers");
+			}
+			for (let i = 0; i < values.length; i += 1){
+				this.writeByte(values[i], i, true);
+			}
+		}else if(typeof values === "number"){
+			const bits = this.rCount * 8;
+			let shiftValue = 0xff;
+			let register = 0;
+			for (let i = 0; i < bits; i += 8){
+				this.writeByte((values & shiftValue) >> i, register, true);
+				register += 1;
+				shiftValue <<= 8;
+			}
+		}else if(typeof values === "bigint"){
+			const bits = BigInt(this.rCount) * 8n;
+			let shiftValue = 0xffn;
+			let register = 0;
+			for (let i = 0n; i < bits; i += 8n){
+				this.writeByte(Number((values & shiftValue) >> i), register, true);
+				register += 1;
+				shiftValue <<= 8n;
+			}
+		}else{
+			throw new TypeError("Values must be an Array, Buffer, Uint8Array, number, or bigint");
+		}
 		this.apply();
 	}
 	write(pin, state){
 		let thing = [];
 		thing[pin] = state;
-		this.writeByte(thing)
+		if (this.rCount === 1){
+			this.writeByte(thing)
+		}else{
+			this.writeState(thing)
+		}
+		
 	}
 }
 module.exports = {ShiftRegister};
